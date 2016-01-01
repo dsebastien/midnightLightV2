20cf98efacbf9ebbce25402245bd5c0e80cff171
Sebastien <seb@dsebastien.net>
2016-01-01 11:29:40 +0100

    Updated boot

--- app/core/boot.ts
+++ app/core/boot.ts
@@ -7,16 +7,19 @@ import {App} from "./app";
 import {bootstrap} from "angular2/platform/browser";
 import {provide} from "angular2/core";
 import {Http, HTTP_PROVIDERS} from "angular2/http";
+import {ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/common_dom';
 
 // import Angular 2 Component Router
 // reference: http://blog.thoughtram.io/angular/2015/06/16/routing-in-angular-2.html
 import {LocationStrategy, PathLocationStrategy, ROUTER_PROVIDERS} from "angular2/router";
 
+// import {enableProdMode} from 'angular2/core';
+
 // app services
 //import {appServicesInjectables} from "core/services/services";
 
 // enable production mode of Angular
-//enableProdMode();
+//enableProdMode(); // enable for production (also uncomment the import above!)
 
 // bootstrap our app
 console.log("Bootstrapping the App");
@@ -26,6 +29,7 @@ bootstrap(App, [
 	//appServicesInjectables, // alternative way of filling the injector with all the classes we want to be able to inject
 	ROUTER_PROVIDERS,
 	HTTP_PROVIDERS,
+	ELEMENT_PROBE_PROVIDERS, // remove in production
 	provide(LocationStrategy, { useClass: PathLocationStrategy }) // enables the following: /#/<component_name> rather than /<component_name>
 
 ]).then(
